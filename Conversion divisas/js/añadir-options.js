caf352f35ddf7081a18172b011e69363607b727f
Santiago Sandoval <sandovalbgasantiago@gmail.com>
2024-01-31 20:38:02 -0500

    finalizado

--- Conversion divisas/js/añadir-options.js
+++ Conversion divisas/js/añadir-options.js
@@ -1,10 +1,12 @@
-async function añadirOptions(){
+export async function añadirOptions(){
     const url = `https://cdn.dinero.today/api/boa.json`
     const respuesta = await fetch(url)
     const json = await respuesta.json()
     let rates = json.rates
     let divisasNombre = Object.keys(rates);
     let divisasValor = Object.values(rates);
+    let i = 0;
+    let j = 0;
     console.log(json.rates)
     console.log(Array(json.rates))
     console.log(Object.keys(rates))
@@ -14,13 +16,18 @@ async function añadirOptions(){
         let div_dest = document.getElementById("div-dest")
         let option = document.createElement("option");
         option.textContent = divisa;
+        option.value=i
         div_dest.appendChild(option);
+        i++
     });
     divisasNombre.forEach(divisa=>{
+
         let div_org = document.getElementById("div-orig")
         let option = document.createElement("option");
         option.textContent = divisa;
+        option.value=j
         div_org.appendChild(option)
+        j++
     })
 }
-añadirOptions()
+
